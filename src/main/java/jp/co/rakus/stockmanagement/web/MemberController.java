aabfb202920ea4cb3a20125fbd413c336d2df8f9
kaneko <shuhei.kaneko@rakus.co.jp>
2017-08-14 15:01:22 +0900

    メンバー登録機能に入力値チェック処理を加えました:w

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,7 +62,12 @@ public class MemberController {
 	 */
 	@RequestMapping(value = "create")
 	public String create(@Validated MemberForm form, 
-			Model model) {
+			BindingResult result, Model model) {
+		
+		if (result.hasErrors()){
+			return "/member/form";
+		}
+		
 		Member member = new Member();
 		BeanUtils.copyProperties(form, member);
 		memberService.save(member);
